5a269c1ffd6958fd4f7af3a68c6931c0cce70248
Ed Miles <edmiles@Eds-MacBook-Pro.lan>
2022-03-07 18:18:50 +0000

    added particles effects

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 // import * as React from 'react';
 import { Typography } from "@mui/material";
 import Layout from "../components/Layout";
-
+import BackgroundParticles from "../components/BackgroundParticles";
 export default function Index() {
+
   return (
     <>
       <Layout>
+        <BackgroundParticles/>
         <Typography variant="h4" component="h1" gutterBottom>
           Landing Page
         </Typography>
