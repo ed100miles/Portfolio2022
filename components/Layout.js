5612d48974a54c3a9c0ee5759ddde65309edd60e
Ed Miles <edmiles@Eds-MacBook-Pro.lan>
2022-03-08 21:35:09 +0000

    fixed next/router hook

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,4 @@
-import { Fragment, useState } from "react";
+import { useState } from "react";
 import {
   AppBar,
   Box,
@@ -17,7 +17,7 @@ import MenuIcon from "@mui/icons-material/Menu";
 import Link from "next/link";
 import BackgroundParticles from "./BackgroundParticles";
 import CustomPaper from "./CustomPaper";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 import theme from "../src/theme";
 import Router from "next/router";
 
@@ -183,7 +183,7 @@ export default function Layout({ children }) {
           animate={{ opacity: 1 }}
           exit={{opacity:0}}
           transition={{
-            duration: 0.2,
+            duration: 0.3,
             ease: "easeInOut",
             type: "tween",
           }}
--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,19 @@
-import * as React from 'react';
-import Head from 'next/head';
-import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import { CacheProvider } from '@emotion/react';
-import theme from '../src/theme';
-import createEmotionCache from '../src/createEmotionCache';
-import {Motion, AnimatePresence} from 'framer-motion'
-import Router from 'next/router';
+import * as React from "react";
+import Head from "next/head";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { CacheProvider } from "@emotion/react";
+import theme from "../src/theme";
+import createEmotionCache from "../src/createEmotionCache";
+import { AnimatePresence } from "framer-motion";
+import { useRouter } from "next/router";
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
 
 export default function MyApp(props) {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
-
+  const router = useRouter();
   return (
     <CacheProvider value={emotionCache}>
       <Head>
@@ -23,7 +23,7 @@ export default function MyApp(props) {
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
         <AnimatePresence exitBeforeEnter>
-          <Component {...pageProps} key={Router.pathname} />
+          <Component {...pageProps} key={router.pathname} />
         </AnimatePresence>
       </ThemeProvider>
     </CacheProvider>
