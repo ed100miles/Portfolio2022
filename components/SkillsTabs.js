6d93263e5286a304d3a33b899845b8fe680b237d
Ed Miles <edmiles@Eds-MacBook-Pro.lan>
2022-03-15 21:44:36 +0000

    technical skills tabs mapped

--- components/SkillsTabs.js
+++ components/SkillsTabs.js
@@ -5,6 +5,7 @@ import Tab from "@mui/material/Tab";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import { useTheme } from "@emotion/react";
+import SkillsCopy from "../public/copy/skills.json";
 
 const TabPanel = (props) => {
   const { children, value, index, ...other } = props;
@@ -18,7 +19,7 @@ const TabPanel = (props) => {
       sx={{
         bgcolor: theme.palette.blue.dark,
         width: "100%",
-        overflow: 'scroll',
+        overflow: "scroll",
       }}
       {...other}
     >
@@ -42,15 +43,12 @@ const a11yProps = (index) => {
     id: `vertical-tab-${index}`,
     "aria-controls": `vertical-tabpanel-${index}`,
   };
-}
+};
 
-export const ProgrammingTabs = () => {
+export const MakeSkillsTabs = ({skill}) => {
   const [value, setValue] = React.useState(0);
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
+  const handleChange = (event, newValue) => setValue(newValue);
   const theme = useTheme();
-
   return (
     <Box
       sx={{
@@ -72,32 +70,17 @@ export const ProgrammingTabs = () => {
           bgcolor: theme.palette.blue.shadow.medium,
         }}
       >
-        <Tab label="Python" {...a11yProps(0)} sx={{color: 'white'}}/>
-        <Tab label="JavaScript" {...a11yProps(1)} sx={{color: 'white'}}/>
-        <Tab label="Bash" {...a11yProps(2)} sx={{color: 'white'}}/>
+        {Object.keys(SkillsCopy[skill]).map((key) => (
+          <Tab label={key} {...a11yProps(0)} sx={{ color: "white" }} />
+        ))}
       </Tabs>
-      <TabPanel value={value} index={0}>
-        This is the programming language I started learning first, around five years
-        ago. Over this time I've develped a thorough understanding of core concepts
-        such as working with different data types, logic and flow control, raising
-        and handling exceptions, effectively using functions and classes, as well as
-        utilising modules from the standard library and wider Python ecosystem.
-        Some examples of libraries I'm familiar with are given under the other tabs
-        above.
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        A good understanding of Python allowed me to pick up JavaScript with relative
-        ease once the syntax was learned. I now enjoy using JavaScript both in the
-        browser and using Node.js, for web development and to provide a better user
-        experience for my projects. I'm comfortable writing asynchronous programs,
-        using the fetchAPI, and I'm familiar with the newer features implemented in
-        ES5 and ES6.
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        I primarily use distributions based on the Linux operating system. As a
-        result I'm at ease working on the CLI, writing bash scripts, and performing
-        system administration tasks with Bash.
-      </TabPanel>
+      {Object.keys(SkillsCopy[skill]).map((key, index) => {
+        return (
+          <TabPanel value={value} index={index}>
+            {SkillsCopy[skill][key]}
+          </TabPanel>
+        );
+      })}
     </Box>
   );
 };
--- pages/skills.js
+++ pages/skills.js
@@ -4,14 +4,15 @@ import {
   Accordion,
   AccordionDetails,
   AccordionSummary,
-  styled
+  styled,
 } from "@mui/material";
 import { useState } from "react";
 import Layout from "../components/Layout";
 import theme from "../src/theme";
-import { ExpandMore} from "@mui/icons-material";
+import { ExpandMore } from "@mui/icons-material";
 import { HomeContentContainer } from ".";
-import { ProgrammingTabs } from "../components/SkillsTabs";
+import { MakeSkillsTabs } from "../components/SkillsTabs";
+import SkillsCopy from "../public/copy/skills.json";
 
 const SkillsAccordionBox = styled(Box, { theme })({
   maxHeight: "60vh",
@@ -32,7 +33,6 @@ const StyledAccordion = styled(Accordion, { theme })({
   backgroundColor: theme.palette.blue.shadow.dark,
 });
 
-
 export default function Skills() {
   const [expanded, setExpanded] = useState(false);
   const handleChange = (panel) => (event, isExpanded) => {
@@ -46,124 +46,25 @@ export default function Skills() {
           Technical Skills
         </Typography>
         <SkillsAccordionBox>
-          <StyledAccordion
-            expanded={expanded === "panel1"}
-            onChange={handleChange("panel1")}
-          >
-            <AccordionSummary
-              expandIcon={<StyledExpandIcon />}
-              aria-controls="panel1bh-content"
-              id="panel1bh-header"
-            >
-              <Typography variant="h4">
-                Programming
-              </Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <ProgrammingTabs/>
-            </AccordionDetails>
-          </StyledAccordion>
-          <StyledAccordion
-            expanded={expanded === "panel2"}
-            onChange={handleChange("panel2")}
-          >
-            <AccordionSummary
-              expandIcon={<StyledExpandIcon />}
-              aria-controls="panel2bh-content"
-              id="panel2bh-header"
-            >
-              <Typography variant="h4">
-                Front End
-              </Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography variant="p2">
-                Donec placerat, lectus sed mattis semper, neque lectus feugiat
-                lectus, varius pulvinar diam eros in elit. Pellentesque convallis
-                laoreet laoreet.
-              </Typography>
-            </AccordionDetails>
-          </StyledAccordion>
-          <StyledAccordion
-            expanded={expanded === "panel3"}
-            onChange={handleChange("panel3")}
-          >
-            <AccordionSummary
-              expandIcon={<StyledExpandIcon />}
-              aria-controls="panel3bh-content"
-              id="panel3bh-header"
-            >
-              <Typography variant="h4">
-                Back End
-              </Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography variant="p2">
-                Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit
-                amet egestas eros, vitae egestas augue. Duis vel est augue.
-              </Typography>
-            </AccordionDetails>
-          </StyledAccordion>
-          <StyledAccordion
-            expanded={expanded === "panel4"}
-            onChange={handleChange("panel4")}
-          >
-            <AccordionSummary
-              expandIcon={<StyledExpandIcon />}
-              aria-controls="panel4bh-content"
-              id="panel4bh-header"
-            >
-              <Typography variant="h4">
-                Data
-              </Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography variant="p2">
-                Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit
-                amet egestas eros, vitae egestas augue. Duis vel est augue.
-              </Typography>
-            </AccordionDetails>
-          </StyledAccordion>
-          <StyledAccordion
-            expanded={expanded === "panel5"}
-            onChange={handleChange("panel5")}
-          >
-            <AccordionSummary
-              expandIcon={<StyledExpandIcon />}
-              aria-controls="panel5bh-content"
-              id="panel5bh-header"
-            >
-              <Typography variant="h4">
-                Version Control
-              </Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography variant="p2">
-                Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit
-                amet egestas eros, vitae egestas augue. Duis vel est augue.
-              </Typography>
-            </AccordionDetails>
-          </StyledAccordion>
-          <StyledAccordion
-            expanded={expanded === "panel6"}
-            onChange={handleChange("panel6")}
-          >
-            <AccordionSummary
-              expandIcon={<StyledExpandIcon />}
-              aria-controls="panel6bh-content"
-              id="panel6bh-header"
-            >
-              <Typography variant="h4">
-                Also Familiar With...
-              </Typography>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Typography variant="p2">
-                Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit
-                amet egestas eros, vitae egestas augue. Duis vel est augue.
-              </Typography>
-            </AccordionDetails>
-          </StyledAccordion>
+          {Object.keys(SkillsCopy).map((skill) => {
+            return (
+              <StyledAccordion
+                expanded={expanded === skill}
+                onChange={handleChange(skill)}
+              >
+                <AccordionSummary
+                  expandIcon={<StyledExpandIcon />}
+                  aria-controls={`${skill}bh-content`}
+                  id={`${skill}bh-header`}
+                >
+                  <Typography variant="h4">{skill}</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  <MakeSkillsTabs skill={skill} />
+                </AccordionDetails>
+              </StyledAccordion>
+            );
+          })}
         </SkillsAccordionBox>
       </HomeContentContainer>
     </Layout>
