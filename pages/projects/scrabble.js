7659367be2891ebddb92ec453fdbd387b5f48808
Ed Miles <edmiles@Eds-MacBook-Pro.lan>
2022-03-24 22:38:15 +0000

    scrabble board controls added

--- pages/projects/scrabble.js
+++ pages/projects/scrabble.js
@@ -3,6 +3,7 @@ import { Typography, Box, styled } from "@mui/material";
 import Layout from "../../components/Layout";
 import theme from "../../src/theme";
 import { motion } from "framer-motion";
+import { MoveDown } from "@mui/icons-material";
 
 const SquareBox = styled(Box)({
   height: "4vmin",
@@ -15,20 +16,64 @@ const SquareBox = styled(Box)({
   "&:hover": {
     cursor: "pointer",
     backgroundColor: theme.palette.blue.shadow.medium,
-  }
-})
+  },
+});
 
 export default function Index() {
   const [board, setBoard] = useState(Array(225).fill(""));
   const [selectedSquare, setSelectedSquare] = useState(112);
+  const [direction, setDirection] = useState("right");
+
+  const setDirectionHandler = (rule) => {
+    if (rule === "moveLeft") {
+      setSelectedSquare(selectedSquare - 1);
+    } else if (rule === "moveRight") {
+      setSelectedSquare(selectedSquare + 1);
+    } else if (rule === "moveUp") {
+      setSelectedSquare(selectedSquare - 15);
+    } else if (rule === "moveDown") {
+      setSelectedSquare(selectedSquare + 15);
+    } else if (direction === "right" && (selectedSquare + 1) % 15 !== 0) {
+      setSelectedSquare(selectedSquare + 1);
+    } else if (direction === "down" && selectedSquare < 210) {
+      setSelectedSquare(selectedSquare + 15);
+    }
+  };
 
   const keyUpHandler = (e) => {
-    const newBoard = [...board]
-    newBoard[selectedSquare] = e.key
-    setBoard(newBoard)
-    setSelectedSquare(selectedSquare+1)
-    console.log(e.key)
-  }
+    console.log(e.key);
+    const isLetter = e.key.match(/^[A-z]$/g) !== null;
+    const newBoard = [...board];
+    if (isLetter) {
+      newBoard[selectedSquare] = e.key;
+      setBoard(newBoard);
+      setDirectionHandler();
+    } else if (e.key === "Backspace") {
+      newBoard[selectedSquare] = "";
+      setBoard(newBoard);
+      if (direction === "right" && selectedSquare % 15 !== 0) {
+        setDirectionHandler("moveLeft");
+      } else if (direction === "down" && selectedSquare > 14) {
+        setDirectionHandler("moveUp");
+      }
+    }
+    switch (e.key) {
+      case "ArrowLeft": {
+        if (selectedSquare !== 0) {
+          setDirectionHandler("moveLeft");
+        }
+      }
+      case "ArrowRight": {
+        if (selectedSquare !== 225){
+          setDirectionHandler('moveRight')
+        }
+      }
+      case 'ArrowDown': {
+        if (selectedSquare <210){setDirectionHandler('moveDown')}
+      }
+      // TODO: MoveUP
+    }
+  };
 
   const Square = ({ index, letter, selected }) => {
     return (
@@ -36,9 +81,9 @@ export default function Index() {
         key={`squareBox-${index}`}
         sx={{
           borderLeft:
-          index % 15 == 0 ? `solid 1px ${theme.palette.blue.shadow.medium}` : "",
+            index % 15 == 0 ? `solid 1px ${theme.palette.blue.shadow.medium}` : "",
           borderBottom:
-          index > 209 ? `solid 1px ${theme.palette.blue.shadow.medium}` : "",
+            index > 209 ? `solid 1px ${theme.palette.blue.shadow.medium}` : "",
         }}
         component={motion.div}
         initial={selected ? { backgroundColor: theme.palette.blue.dark } : {}}
@@ -48,15 +93,9 @@ export default function Index() {
           repeatType: "reverse",
           duration: 1,
         }}
-        onClick={()=>setSelectedSquare(index)}
+        onClick={() => setSelectedSquare(index)}
       >
-        <Typography
-          sx={{
-            color: theme.palette.blue.light
-          }}
-        >
-          {letter}
-        </Typography>
+        <Typography sx={{ color: theme.palette.blue.light }}>{letter}</Typography>
       </SquareBox>
     );
   };
@@ -71,12 +110,12 @@ export default function Index() {
             alignItems: "center",
             flexWrap: "wrap",
             flexDirection: "column",
-            '&[tabindex]':{
-              outline: 'none'
-            }
+            "&[tabindex]": {
+              outline: "none",
+            },
           }}
           onKeyUp={(e) => keyUpHandler(e)}
-          tabIndex='1'
+          tabIndex="1"
         >
           <Typography variant="h2" component="h1" gutterBottom>
             Scrabble
